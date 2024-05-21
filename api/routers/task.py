c420f48ad2707482f178134d01f4694a9ff00b3d
Yuki <kassyan323@gmail.com>
2024-05-21 16:56:06 +0900

    main

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,4 +1,29 @@
 from fastapi import APIRouter
 
 router = APIRouter()
+
+@router.get("tasks")
+def get_tasks():
+    pass
+
+@router.post("tasks")
+def create_task():
+    pass
+
+@router.put("tasks/{task_id}/todo")
+def task_todo(task_id: int):
+    pass
+
+@router.put("tasks/{task_id}/done")
+def task_done(task_id: int):
+    pass
+
+@router.put("tasks/{task_id}/doing")
+def task_doing(task_id: int):
+    pass
+
+@router.delete("tasks/{task_id}")
+def delete_task(task_id: int):
+    pass
+
     
